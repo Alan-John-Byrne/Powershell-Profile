7ee5ae79bee5b46cfeb3e750f55254adba595ccd
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2024-06-13 22:56:22 +0100

    Altered credential create script slightly.

--- Scripts/create-self-signed-cert-pfx-file.ps1
+++ Scripts/create-self-signed-cert-pfx-file.ps1
@@ -22,18 +22,21 @@ exit
 "@
 
 # Use Start-Process to call this command in an elevated PowerShell process
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command $command" -Verb RunAs
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command $command" -Verb RunAs -Wait
 
-# Writing non-sensitive details to a text file in the current directory. (CANNOT BE DONE WITHIN ELEVATED PROMPT: THE PIPE (|) IS NOT ALLOWED IN ELEVATED PROMPT)
-$outputFile = Join-Path -Path $(Get-Location) -ChildPath "CertificateDetails.txt"
+# Define the path and filename for the output text file
+$outputFileName = "CertificateDetails.txt"
+$outputFilePath = Join-Path (Split-Path -Path $PfxFilePath) $outputFileName
+
+# Writing non-sensitive details to a text file in the directory of the PFX file
 $outputContent = "Subject Name: $SubjectName`nPFX File Path: $PfxFilePath"
-$outputContent | Out-File -FilePath $outputFile -Encoding UTF8
-Write-Host "Non-sensitive details written to $outputFile"
+$outputContent | Out-File -FilePath $outputFilePath 
+Write-Host "Non-sensitive details written to $outputFilePath"
 
 # Note: The PFX file (also known as a PKCS, for 'Personal Key Cryptography Standards'
 # file) is used for storeing private keys, and certificates, and optionally a 
 # certificate chain.
 
-# Note2: A PFX / PKCS file is used for securely storing your certificate and private 
+# Note2: A PFX / PKCS file is used for securely storing your certificate and private
 # keys, which is necessary for signing code. You can also transfer them between 
 # machines, or different applications that use the certificate.
