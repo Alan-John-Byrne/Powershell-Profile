6c50addbfa46ffc7845b3ae4715c3948d84ddf4c
Alan-John-Byrne <alanjohnbarnesbyrne@gmail.com>
2024-10-15 01:42:37 +0100

    feat(virtual-env): Add python virtual environment support
    I've added support for powershell to recognise when the user enters a python virtual environment.

--- Scripts/appearance.ps1
+++ Scripts/appearance.ps1
@@ -1,36 +1,50 @@
 # Initial Setup:
+
+# IMPORTANT: First checking to see if there is a virtual environment for python activated.
+$virtual_env = ""
+if ($env:VIRTUAL_ENV)
+{
+  $virtual_env = "Python-Virtual-Env->($($env:VIRTUAL_ENV.Split("\")[-1]))" #  NOTE: Extracting out the name of the virtual environment.
+}
+
+# NOTE: MAIN APPEARANCE SCRIPT:
 $currentDrive = $PWD.Drive.Name
 $currentLocation = Get-Location
 # Different Parts of the prompt (each 'Write-Host' is a different segment of the prompt.)
-if ($currentDrive -eq 'C') {
-# Steps needed prior for drive C:
+if ($currentDrive -eq 'C')
+{
+
+  # Steps needed prior for drive C:
   $indexOfChar = $PWD.Path.IndexOf("\", $PWD.Path.IndexOf("\") + 1)
-    $userNameFromPath = $PWD.Path.Substring($indexOfChar + 1)
-    $indexOfUsername = $currentLocation.Path.IndexOf($userNameFromPath)
-    $currentPath = $currentLocation.Path.Substring($indexOfUsername)
-# Check for if the username is in the current path, otherwise we are in root.
-    if ($PWD.Path.Contains($(get-username))) {
-      Write-Host "$currentDrive`:\\ " -ForegroundColor Green -NoNewline
-        Write-Host "@ User" -ForegroundColor Gray -NoNewline
-        Write-Host " -> " -ForegroundColor Yellow -NoNewline
-        Write-Host "$currentPath" -ForegroundColor Cyan -NoNewline # Username in path (Necessary)
-    }
-    else {
-      $path = "$currentDrive`:\\ ~ root" + $PWD.Path.Insert(2, "\")
-        Write-Host "$($path.Substring(0, 11)) " -ForegroundColor Green -NoNewline # Green
-        Write-Host "$($path.Substring(14))" -ForegroundColor Cyan -NoNewline # Cyan
-    }
-}
-else {
+  $userNameFromPath = $PWD.Path.Substring($indexOfChar + 1)
+  $indexOfUsername = $currentLocation.Path.IndexOf($userNameFromPath)
+  $currentPath = $currentLocation.Path.Substring($indexOfUsername)
+  # Check for if the username is in the current path, otherwise we are in root.
+  if ($PWD.Path.Contains($(get-username)))
+  {
+    Write-Host "$currentDrive`:\\ $($virtual_env) " -ForegroundColor Green -NoNewline # IMPORTANT: If we are in a virtual environment, prepend that virtual environment name:
+    Write-Host "@ User" -ForegroundColor Gray -NoNewline
+    Write-Host " -> " -ForegroundColor Yellow -NoNewline
+    Write-Host "$currentPath" -ForegroundColor Cyan -NoNewline # Username in path (Necessary)
+  } else
+  {
+    $path = "$currentDrive`:\\ $($virtual_env) ~ root" + $PWD.Path.Insert(2, "\")
+    Write-Host "$($path.Substring(0, 11)) " -ForegroundColor Green -NoNewline # Green
+    Write-Host "$($path.Substring(14))" -ForegroundColor Cyan -NoNewline # Cyan
+  }
+} else
+{
+
   $indexOfChar = $currentLocation.Path.IndexOf("\")
-    $currentPath = $currentLocation.Path.Substring($indexOfChar + 1)
-    if ($currentPath) {
-      Write-Host "$currentDrive`:\\ ~ root " -ForegroundColor Green -NoNewline # Green
-        Write-Host "\$currentPath" -ForegroundColor Cyan -NoNewline # Cyan
-    }
-    else {
-      Write-Host "$currentDrive`:\\ ~ root" -ForegroundColor Green -NoNewline # Green
-    }
+  $currentPath = $currentLocation.Path.Substring($indexOfChar + 1)
+  if ($currentPath)
+  {
+    Write-Host "$currentDrive`:\\ $($virtual_env) ~ root " -ForegroundColor Green -NoNewline # Green # IMPORTANT: If we are in a virtual environment, prepend that virtual environment name:
+    Write-Host "\$currentPath" -ForegroundColor Cyan -NoNewline # Cyan
+  } else
+  {
+    Write-Host "$currentDrive`:\\ $($virtual_env) ~ root" -ForegroundColor Green -NoNewline # Green
+  }
 }
 # Nice little design:
 Write-Host " >>" -ForegroundColor Blue -NoNewline # Blue
